cba90e5def5b6a8a2d9580b04b4f5b9b10077fcc
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2018-12-04 11:07:12 +0530

    Add files via upload

---
+++ pascal.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int i,k,x,j=5;
+	for(i=1;i<=5;i++)
+	  
+	{
+		for(k=1;k<=j;k++)
+		{
+		  cout<<" ";
+	    }
+	for(x=1;x<=i;x++)
+	 {
+		cout<<i;
+		cout<<" ";
+   	 }
+	cout<<endl;
+	j=j-1;
+    }
+    return 0;
+}
