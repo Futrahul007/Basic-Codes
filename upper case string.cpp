0f06e2f3f71b9466a602210f4b2e57e9efcb1468
Rahul Raj <42494150+Futrahul007@users.noreply.github.com>
2018-12-04 10:48:28 +0530

    Add files via upload

---
+++ upper case string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	char str[40];
+	cout<<"Enter the string in Lower case"<<endl;
+	cin>>str;
+	strupr(str);
+	cout<<"\n Upper String are = "<<str; 
+}
